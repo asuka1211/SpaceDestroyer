6b8c9c2b6b1645abe7f3a265a53fe13eb10245fd
asuka1211 <maks1211@bk.ru>
2023-11-26 21:45:43 +0600

    add shooter mechanics

---
+++ Laser.cs
@@ -0,0 +1,21 @@
+using Godot;
+using System;
+
+public partial class Laser : Area2D
+{
+
+    private int _speed = 10;
+
+    [Export]
+    public Vector2 Direction;
+
+    public override void _PhysicsProcess(double delta)
+    {
+        GlobalPosition += _speed * Direction;
+    }
+
+    private void _OnVisibleOnScreenNotifier2dScreenExited()
+    {
+        QueueFree();
+    }
+}
--- Player.cs
+++ Player.cs
@@ -4,15 +4,68 @@ using System;
 public partial class Player : CharacterBody2D
 {
 
-    private int speed = 400;
+    private int _speed = 400;
+    private AnimationPlayer _animationPlayer;
+    private PackedScene _laser = ResourceLoader.Load("res://laser.tscn") as PackedScene;
+    private Marker2D _gun1;
+    private Marker2D _gun2;
+    private Marker2D _gun3;
+    private Marker2D _gun4;
+    public override void _Ready()
+    {
+        _animationPlayer = GetNode<AnimationPlayer>("AnimationPlayer");
+        _gun1 = GetNode<Marker2D>("LaserSpawn1");
+        _gun2 = GetNode<Marker2D>("LaserSpawn2");
+        _gun3 = GetNode<Marker2D>("LaserSpawn3");
+        _gun4 = GetNode<Marker2D>("LaserSpawn4");
+    }
 
     public override void _PhysicsProcess(double delta)
     {
+        Navigation();
+    }
+
+
+    public override void _Process(double delta)
+    {
+        Shoot();
+    }
+
+    private void Navigation()
+    {
         var target = GetGlobalMousePosition();
         var direction = (target - GlobalPosition).Normalized();
-        var velocity = direction * speed;
-        Velocity = velocity;
-        MoveAndSlide();
+        Velocity = direction * _speed;
+        if (Position.DistanceTo(target) > 10)
+        {
+            MoveAndSlide();
+            _animationPlayer.Play("fly");
+        }
+        else
+        {
+            _animationPlayer.Stop();
+        }
         LookAt(target);
+    }
+
+    private void Shoot()
+    {
+        if (Input.IsActionJustPressed("shoot"))
+        {
+            InstantiateLaser(_gun1);
+            InstantiateLaser(_gun2);
+            InstantiateLaser(_gun3);
+            InstantiateLaser(_gun4);
+        }
+    }
+
+    private void InstantiateLaser(Marker2D gun)
+    {
+        var laser = _laser.Instantiate() as Laser;
+        var direction = GlobalPosition.DirectionTo(GetGlobalMousePosition());
+        laser.GlobalPosition = gun.GlobalPosition;
+        laser.Direction = direction.Normalized();
+        laser.Rotation = direction.Angle();
+        GetTree().CurrentScene.AddChild(laser);
     }
 }
